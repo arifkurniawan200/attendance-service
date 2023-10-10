324812a1a89ed8be0c4205d5a935fb4ab1b92c3b
arif kurniawan <arifkurniawan98@outlook.com>
2023-10-10 20:47:01 +0700

    refactor: change creator using user id

--- db/migration/20231010174613_create_table_gathering.sql
+++ db/migration/20231010174613_create_table_gathering.sql
@@ -2,14 +2,15 @@
 -- +goose StatementBegin
 CREATE TABLE gatherings (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    creator VARCHAR(255) NOT NULL,
+    creator INT NOT NULL,
     type ENUM('online', 'offline') NOT NULL,
     schedule_at DATETIME NOT NULL,
     name VARCHAR(255) NOT NULL,
     location VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP DEFAULT NULL
+    deleted_at TIMESTAMP DEFAULT NULL,
+    FOREIGN KEY (creator) REFERENCES members(id),
 );
 -- +goose StatementEnd
 
