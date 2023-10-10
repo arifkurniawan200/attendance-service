26842d8ac85c103928bbdd0960ce1567f9bb3825
arif kurniawan <arifkurniawan98@outlook.com>
2023-10-10 19:38:31 +0700

    feat: add query for create new gathering

--- internal/repository/queries.go
+++ internal/repository/queries.go
@@ -7,4 +7,8 @@ const (
 			SELECT id, first_name, last_name, email,password, created_at, updated_at,deleted_at
 	FROM members WHERE email = ?
     `
+	createNewGathering = `
+INSERT INTO gatherings (creator, type, schedule_at, name, location)
+VALUES (?, ?, ?, ?, ?);
+`
 )
